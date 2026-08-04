2d13de39d1077e8bffcca7cfe2fa163d28c03204
Pedrodev <pedroarthurnf00711@gmail.com>
2026-08-04 17:32:10 -0300

    Começei o projeto cli de TS

---
+++ Typescript/Projeto-cli/src/index.ts
@@ -0,0 +1,13 @@
+// Menu / Interação
+
+import * as readline from 'readline';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Qual seu nome? ', (resposta) => {
+    console.log(`Olá, ${resposta}!`);
+    rl.close();
+});
---
+++ Typescript/Projeto-cli/src/utils/arquivo.ts
@@ -0,0 +1,1 @@
+// Ler / salvar JSON
