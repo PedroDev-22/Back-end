9f0336635429998b7da46b1ddaf743453a257c26
Pedrodev <pedroarthurnf00711@gmail.com>
2026-08-11 17:20:26 -0300

    Começei Node.JS

---
+++ NodeJS/01-Fundamentos-Node/01-event-loop.d.ts
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=01-event-loop.d.ts.map
---
+++ NodeJS/01-Fundamentos-Node/01-event-loop.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("1. O garçom anota o pedido (Inicio)");
+setTimeout(() => {
+    console.log("2. O prato ficou pronto e foi entregue (Callback do Timer)");
+}, 0);
+console.log("3. O garçom atende outro cliente (Fim)");
+//# sourceMappingURL=01-event-loop.js.map
