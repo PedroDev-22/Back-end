a1c7d7c1f53689526a64cbb307cbf55a0e4eb884
Pedrodev <pedroarthurnf00711@gmail.com>
2026-08-15 19:13:37 -0300

    Continuei o módulo 2 de Node.js

---
+++ NodeJS/02-http-nativo/dist/experimentos/03-methods.js
@@ -0,0 +1,18 @@
+import http from 'node:http';
+const server = http.createServer((request, response) => {
+    const { method, url } = request;
+    if (url === '/vendas') {
+        if (method === 'GET') {
+            response.writeHead(201, { "content-type": 'application/json' }); // 201 => criado
+            return response.end(JSON.stringify({ message: "aqui estão as vendas" }));
+        }
+        if (method === 'POST') {
+            response.writeHead(201, { "content-type": 'application/json' }); // 201 => criado
+            return response.end(JSON.stringify({ message: "Venda criada com sucesso" }));
+        }
+    }
+    response.writeHead(404, { "content-type": "text/plain; charset=utf-8" });
+    response.end(`Rota não encontrada`);
+});
+server.listen(3000, () => console.log("Servidor rodando na porta 3000"));
+//# sourceMappingURL=03-methods.js.map
---
+++ NodeJS/02-http-nativo/experimentos/04-body-stream.ts
@@ -0,0 +1,30 @@
+import http from 'node:http';
+
+const server = http.createServer((request, response) => {
+    const { method, url } = request;
+
+    if (url === '/vendas' && method === 'POST') {
+        const chunks: Buffer[] = [];
+
+        request.on('data', (chunk) => {
+            chunks.push(chunk);
+        })
+
+        request.on('end', () => {
+            const bufferUnico = Buffer.concat(chunks).toString();
+            const body = JSON.parse(bufferUnico);
+
+            console.log("Corpo recebido", body)
+            response.writeHead(201, { "content-type": "applications/json" });
+            response.end(JSON.stringify({
+                message: "Venda recebida!",
+                dados: body,
+            }))
+        });
+    } else {
+        response.writeHead(404, { "content-type": "text/plain; charset=utf-8" });
+        response.end("Não encontrado");
+    }
+});
+
+server.listen(3000, () => console.log("Servido rodando na porta 3000"));
